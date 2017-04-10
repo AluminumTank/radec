9d0f5beb74f77932741cbb418816fcaee47317fb
Joel Beckmeyer <brainboggler97@gmail.com>
2017-04-10 19:38:54 -0400

    add some functionality to andGate class

---
+++ src/andGate.cpp
@@ -0,0 +1,11 @@
+#include "andGate.h"
+
+using namespace std;
+
+andGate::andGate(wire* wire1 = nullptr, wire* wire2 = nullptr,
+		wire* wire3 = nullptr) : in1(wire1), in2(wire2), out(wire3) {}
+
+int andGate::evaluate(int evTime) {
+	int bool1 = in1 -> getValue();
+	return ( in1 -> getValue(evTime) ) && ( in2 -> getValue(evTime) );
+}
--- src/event.cpp
+++ src/event.cpp
@@ -5,6 +5,7 @@ using namespace std;
 event::event(event, value, setTime, wire) : evNum(event), evValue(value),
 	evTime(setTime), wireNum(wire) {}
 
+// operator overload so that priority_queue will function correctly
 bool event::operator<(const event &e1, const event &e2) {
 	if(e1.evTime == e2.evTime) {
 		return e1.evNum > e2.evNum;
