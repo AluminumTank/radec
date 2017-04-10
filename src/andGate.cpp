53198547c03bb77ccea59000709b0327b1792c6f
Joel Beckmeyer <brainboggler97@gmail.com>
2017-04-10 19:49:35 -0400

    add simple evaluation to andGate

--- src/andGate.cpp
+++ src/andGate.cpp
@@ -6,6 +6,9 @@ andGate::andGate(wire* wire1 = nullptr, wire* wire2 = nullptr,
 		wire* wire3 = nullptr) : in1(wire1), in2(wire2), out(wire3) {}
 
 int andGate::evaluate(int evTime) {
-	int bool1 = in1 -> getValue();
-	return ( in1 -> getValue(evTime) ) && ( in2 -> getValue(evTime) );
+	int val1 = in1->getValue();
+	int val2 = in2->getValue();
+	if(val1 != -1 && val2 != -1) {
+		return val1 && val2;
+	}
 }
