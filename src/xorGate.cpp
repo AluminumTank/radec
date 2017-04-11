7574ffc9a4e4a7587545c9d8442663ce14d0a5cf
daniel <daniel12997@gmail.com>
2017-04-10 21:28:44 -0400

    Fix base initialization for and, not , and xor

--- src/xorGate.cpp
+++ src/xorGate.cpp
@@ -3,8 +3,13 @@
 using namespace std;
 
 xorGate::xorGate(priority_queue<event> *eQueue, int d, wire* wire1,
-		wire* wire2, wire* wire3) : e(eQueue), delay(d), in1(wire1), in2(wire2),
-		out(wire3);
+	wire* wire2, wire* wire3) {
+	e = eQueue;
+	delay = d;
+	in1 = wire1;
+	in2 = wire2;
+	out = wire3;
+}
 
 void xorGate::evaluate(int evTime) {
 	//TODO
