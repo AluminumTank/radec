010d572e0a7a9659bf7fe817e1d18f56257b8da2
Joel Beckmeyer <brainboggler97@gmail.com>
2017-04-10 21:28:10 -0400

    fix stupid mistake

--- src/norGate.cpp
+++ src/norGate.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 norGate::norGate(priority_queue<event> *eQueue, int d, wire* wire1, wire* wire2,
-		wire* wire3) : e(eQueue), delay(d), in1(wire1), in2(wire2), out(wire3);
+		wire* wire3) {
+	e = eQueue;
+	delay = d;
+	in1 = wire1;
+	in2 = wire2;
+	out = wire3;
+}
 
 void norGate::evaluate(int evTime) {
 	//TODO
--- src/xnorGate.cpp
+++ src/xnorGate.cpp
@@ -3,8 +3,13 @@
 using namespace std;
 
 xnorGate::xnorGate(priority_queue<event> *eQueue, int d, wire* wire1,
-		wire* wire2, wire* wire3) : e(eQueue), delay(d), in1(wire1), in2(wire2),
-		out(wire3);
+		wire* wire2, wire* wire3) {
+	e = eQueue;
+	delay = d;
+	in1 = wire1;
+	in2 = wire2;
+	out = wire3;
+}
 
 void xnorGate::evaluate(int evTime) {
 	//TODO
