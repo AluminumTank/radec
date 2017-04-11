c19ba283207c1a181c24cc49c23621b7590d7b1b
Joel Beckmeyer <brainboggler97@gmail.com>
2017-04-10 20:05:02 -0400

    begin implementation of notGate

--- src/notGate.cpp
+++ src/notGate.cpp
@@ -1,3 +1,14 @@
 #include "notGate.h"
 
 using namespace std;
+
+notGate::notGate(int gateDelay, wire* wire1 = nullptr, wire* wire2 = nullptr)
+	: delay(gateDelay), in1(wire1), out(wire2) {}
+
+notGate::evaluate() {
+	int val = in1->getValue();
+
+	if(val != -1) {
+		return !val;
+	}
+}
