8aacd56b65f9ca7fd04e53c427f7430ecd38ae9c
daniel <daniel12997@gmail.com>
2017-04-10 20:44:48 -0400

    Add the event generation to the nand Gate

--- src/event.h
+++ src/event.h
@@ -1,13 +1,16 @@
 #ifndef EVENT
 #define EVENT
+using namespace std;
+#include "wire.h"
 
 
 class event {
 	public:
-		event(int num, int value, int setTime, int wire);
+		event(int num, int value, int setTime, wire * output);
 		bool operator<(const event &e1);
 	private:
-		int evNum, evValue, evTime, wireNum;
+		int evNum, evValue, evTime;
+		wire *out;
 };
 
 #endif // !EVENT
