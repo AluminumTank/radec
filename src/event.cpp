c1c31af8e67abb391d4c388da2cd95d168d36490
daniel <daniel12997@gmail.com>
2017-04-10 20:44:48 -0400

    Add the event generation to the nand Gate

--- src/event.cpp
+++ src/event.cpp
@@ -1,9 +1,7 @@
 #include "event.h"
 
-using namespace std;
-
-event::event(int num, int value, int setTime, int wire) : evNum(num), evValue(value),
-	evTime(setTime), wireNum(wire) {}
+event::event(int num, int value, int setTime, int wire, wire *output) : evNum(num), evValue(value),
+	evTime(setTime), out(output)  {}
 
 bool event::operator<(const event &e1) {
 	if(evTime == e1.evTime) {
--- src/nandGate.cpp
+++ src/nandGate.cpp
@@ -13,11 +13,12 @@ void nandGate::evaluate(int time)
 {
 	if (in1->getValue(time) == 0 || in2->getValue(time) == 0) {
 		if (out->getValue(time + delay) != 1) {
-
+			e->push(event(e->size, 1, time + delay, out));
 			out->setValue(1, time + delay);
 		}
 	}
 	else {
+		e->push(event(e->size, 0, time + delay, out));
 		out->setValue(0, time + delay);
 	}
 }
