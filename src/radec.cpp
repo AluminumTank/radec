372466027aafbc8f71abd4bad02f029c4eae7745
Joel Beckmeyer <brainboggler97@gmail.com>
2017-04-26 11:31:40 -0400

    fix grammar errors, printing errors, and bug with default simulation time

--- src/radec.cpp
+++ src/radec.cpp
@@ -14,30 +14,34 @@ int main() {
 	string fileName;
 	Simulation e;
 	int len = 60;
+	bool canOpen;
 
 	cout << "Please enter filename: ";
 	getline(cin, fileName);
-	cout << "How long do you want to simulate to (default 60)? ";
+	cout << "What time do you want to simulate to (default 60ns)? ";
 	if (cin.peek() == '\n') {
 		len = 60;
 	}
 	else if (!(cin >> len)) {
-		cout << "Invalid input using 60.\n";
+		cout << "Invalid input; using 60.\n";
+		len = 60;
 	}
-	e.parseCircuit(fileName);
+	canOpen = e.parseCircuit(fileName);
 
 	// 2. Parse the vector file to initialize the simulation Queue with initial
 	//		Wire state (i.e., value) changes
-	e.parseVector(fileName);
+	canOpen = e.parseVector(fileName);
 
 	// 3. Simulate the circuit using Event-driven control
 	// first, remove the top Event e in the Queue
 	// second, determine if e causes a future Wire state change
 	// third, create and queue any future Wire state changes as new Events
 	// fourth, apply e's effects
-	e.simulate(len);
+	if(canOpen) {
+		e.simulate(len);
 
-	// 4. Print the results of the simulation
-	e.print(len);
-	system("pause");
+		// 4. Print the results of the simulation
+		e.print(len);
+		system("pause");
+	}
 }
